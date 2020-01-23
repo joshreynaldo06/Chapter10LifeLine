4a4752eaab13777640b7318b03a30bb38b9152e2
Joshua Ronaldo <joshuaronaldo088@gmail.com>
2020-01-23 12:42:53 +0700

    add story and story brain model

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,9 +14,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+//    let story0 = "You see a fork in the road"
+//    let choice1 = "Take a left"
+//    let choice2 = "Take a right"
+    
+    var storyBrain = StoryBrain()
+  
+    
+    @IBAction func buttonClicked(_ sender: UIButton){
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+              
+        updateUI()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
+        
+    }
+    
+    func updateUI(){
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
     }
 
 
